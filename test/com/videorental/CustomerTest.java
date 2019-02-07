dc6557d1ab92c47da75916b475d43038122f1f1f
youmebae <youme.bae@samsung.com>
2019-02-07 20:58:00 +0900

    [Test Case 03 ] Amount 계산 로직 테스트 : Regular movie (1/2)
    if 조건 분기 경계값으로 테스트

--- test/com/videorental/CustomerTest.java
+++ test/com/videorental/CustomerTest.java
@@ -30,6 +30,27 @@ public class CustomerTest {
 	        + "Amount owed is 0.0\n"
 	        + "You earned 0 frequent renter pointers"));
 	}
+	
+	@Test 
+	public void statementForRegularMovieRentalForLessThan3Days() {
+
+	    // arrange
+	    Customer customer = new Customer("NAME_NOT_IMPORTANT");
+	    Movie movie = new Movie("TITLE_NOT_IMPORTANT", Movie.REGULAR);
+	    int daysRented = 2;
+	    Rental rental = new Rental(movie, daysRented);
+	    customer.addRental(rental);
+
+	    // act
+	    String statement = customer.statement();
+
+	    // assert
+	    assertThat(statement, is("Rental Record for NAME_NOT_IMPORTANT\n" 
+	        + "\t2.0(TITLE_NOT_IMPORTANT)\n" 
+	        + "Amount owed is 2.0\n"
+	        + "You earned 1 frequent renter pointers"));
+	} 
+
 
 	
 }

55d8d053f3161fd109fe8dc88e90169dff9fe777
youmebae <youme.bae@samsung.com>
2019-02-07 20:55:11 +0900

    [Test Case 01 ] Customer 생성 테스트
    Customer 객체를 생성하는 테스트를 작성한다.

--- test/com/videorental/CustomerTest.java
+++ test/com/videorental/CustomerTest.java
@@ -1,10 +1,19 @@
 package com.videorental;
 
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class CustomerTest {
+	
+	@Test
+	public void returnNewCustomer()	{
+		Customer customer = new Customer("NAME_NOT_IMPORTANT");
+	    
+	    assertThat(customer, is(notNullValue()));
+	}
 	
 }

0a078835c83e9fd82e2decf91978b4d4a5f0717b
youme <youme.bae@gmail.com>
2019-02-12 15:11:58 +0900

    Update Rental.java

--- src/com/videorental/Rental.java
+++ src/com/videorental/Rental.java
@@ -1,7 +1,7 @@
 package com.videorental;
 
 class Rental {
-	private Movie movie;
+		private Movie movie;
 	private int daysRented;
 
 	public Rental(Movie movie, int daysRented) {
@@ -15,5 +15,27 @@ class Rental {
 
 	public Movie getMovie() {
 		return movie;
+	}
+
+	double getCharge() {
+		double thisAmount = 0;
+		switch (getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (getDaysRented() > 2)
+				thisAmount += (getDaysRented() - 2) * 1.5;
+			break;
+			
+		case Movie.NEW_RELEASE:
+			thisAmount += getDaysRented() * 3;
+			break;
+			
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (getDaysRented() > 3)
+				thisAmount += (getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
 	}
 }

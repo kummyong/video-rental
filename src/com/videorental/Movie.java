411fbde3c60dbbbedc43df57ede77b310679e0bc
youme <youme.bae@gmail.com>
2019-02-12 17:16:32 +0900

    Update Movie.java

--- src/com/videorental/Movie.java
+++ src/com/videorental/Movie.java
@@ -1,6 +1,6 @@
 package com.videorental;
 
-public class Movie {
+public abstract class Movie {
 	public static final int CHILDRENS = 2;
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
@@ -25,25 +25,7 @@ public class Movie {
 		return title;
 	}
 
-	double getChargeFor(int daysRented) {
-		double thisAmount = 0;
-		switch (getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (daysRented > 2)
-				thisAmount += (daysRented - 2) * 1.5;
-			break;
-			
-		case Movie.NEW_RELEASE:
-			thisAmount += daysRented * 3;
-			break;
-			
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (daysRented > 3)
-				thisAmount += (daysRented - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
-	}
+	abstract double getChargeFor(int daysRented);
+
+	abstract int getFrequentRenterPoints(int daysRented);
 }
